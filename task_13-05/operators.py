2df435b7596c334f1ffd868fc450a35758a1d598
meghanath036 <meghanathmegha0143@gmail.com>
2026-06-24 22:57:22 +0530

    operators

---
+++ task_13-05/operators.py
@@ -0,0 +1,69 @@
+#operators
+
+# Arithematic operators
+x=10
+y=20
+print(x+y)
+print(x-y)      
+print(x*y) 
+print(x/y)
+print(x//y)
+print(x%y)  
+print(x**y)
+
+# Assinment operators
+x=30
+y=40
+x+=y
+print(x)
+x-=y
+print(x)
+x*=y
+print(x)
+x/=y
+print(x)
+x%=y
+print(x)
+x**=y
+print(y)
+# relational operators
+a=65
+b=58
+a>b
+print(a<b)
+print(a<b)
+print(a<=b)
+print(a>=b)
+print(a==b)
+print(a!=b)
+# logical operators
+x=5
+y=10
+z=15
+print (x<y)and (x>y)and (x<z)
+print(x<y)or (x>y)or (x<z)
+print(not(x<y))
+
+# bit wise operators
+x=10
+y=20
+print(x&y)
+print(x|y)
+print(x^y)
+print(~x)
+print(x<<2)
+print(x>>2)
+
+# membership operators
+
+x=(1,2,3,4,5)
+print(3 in x)
+print(6 in x)
+print(3 not in x)
+
+# identify operators
+
+x=10
+y=10
+print(x is y)
+print(x is not y)
