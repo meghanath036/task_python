7c03742024b1aa77120e91883c6362190956057b
meghanath036 <meghanathmegha0143@gmail.com>
2026-06-25 11:16:23 +0530

    rename file names

---
+++ task_18-05/io_functions.py
@@ -0,0 +1,48 @@
+# 1. ValueError
+age = int(input("Enter age: "))
+
+# Input:
+# text
+# abc
+
+
+'''Output:
+
+text
+ValueError: invalid literal for int()'''
+
+
+### 2. EOFError
+
+# Occurs when no input is provided.
+
+# python
+name = input("Enter name: ")
+
+
+'''Output:
+
+text
+EOFError: EOF when reading a line'''
+
+
+## Output Errors
+
+### 1. TypeError
+
+# Occurs when incompatible data types are combined.
+
+age = 20
+print("Age = " + age)
+
+
+'''Output:
+text
+TypeError: can only concatenate str (not "int") to str'''
+
+
+# Correct:
+
+# python
+print("Age =", age)
+
