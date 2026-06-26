f58c25109d84d8570416166cd157109ed6ab64db
meghanath036 <meghanathmegha0143@gmail.com>
2026-06-26 22:17:26 +0530

    slicing

---
+++ task_04-06/slicing.py
@@ -0,0 +1,13 @@
+# slicing  of outputs
+l=[1,2,2,3,4,5,6,7,8,9,]
+for i in range(len(l)):
+    print(l[0:6])
+    print(l[6:10 ])
+    print(l[:8])
+    print(l[8:])
+    print(l[-7:])
+    print(l[:-5])
+    print(l[::2])
+    print(l[1:2])
+    print(l[::-1])
+    print(l[2:8:3]) 
