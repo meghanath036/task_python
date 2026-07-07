4cb00ce640a79cab3319388918d41c830f8861ab
meghanath036 <meghanathmegha0143@gmail.com>
2026-07-07 21:55:52 +0530

    leet code

---
+++ 2553.py
@@ -0,0 +1,26 @@
+'''given an array of positive integers nums, return an array answer that consists of the digits of each integer in nums after separating them in the same order they appear in nums.
+
+To separate the digits of an integer is to get all the digits it has in the same order.
+
+For example, for the integer 10921, the separation of its digits is [1,0,9,2,1].
+ 
+
+Example 1:
+
+Input: nums = [13,25,83,77]
+Output: [1,3,2,5,8,3,7,7]
+Explanation: 
+- The separation of 13 is [1,3].
+- The separation of 25 is [2,5].
+- The separation of 83 is [8,3].
+- The separation of 77 is [7,7].
+answer = [1,3,2,5,8,3,7,7]. Note that answer contains the separations in the same order.'''
+
+
+
+nums=[13,34,58,90,89]
+li = []
+for i in nums:
+    for j in str(i):
+        li.append(int(j))
+print( li)
